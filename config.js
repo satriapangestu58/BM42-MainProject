62f2efb43971f312523f053712ae10ccf41897e8
satriapangestu58 <92577525+satriapangestu58@users.noreply.github.com>
2026-08-27 02:10:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Backend v11 menangani scanner dan penilaian.
 // Contoh:
 // const BM42_API_URL = "https://script.google.com/macros/s/DEPLOYMENT_ID/exec";
-const BM42_API_URL = "PASTE_NEW_APPS_SCRIPT_EXEC_URL_HERE";
+const BM42_API_URL = "https://script.google.com/macros/s/AKfycbz_oyHeojKoZroIGP1FUlS-gHPUMEWHL7Rx77c8UKPK9JSGRLeEzCpQYwKZVa9gnJrz/exec";
 const BM42_DEFAULT_EVALUATOR_ID = "SC-01";
 const BM42_STATE_POLL_MS = 30000;
 const BM42_SCAN_COOLDOWN_MS = 2500;
