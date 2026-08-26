59f2870502679c8f0a217e9621eb98d624919e87
satriapangestu58 <92577525+satriapangestu58@users.noreply.github.com>
2026-08-27 04:01:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BM42_API_URL = 'PASTE_APPS_SCRIPT_EXEC_URL_HERE';
+window.BM42_API_URL = 'https://script.google.com/macros/s/AKfycbyaNnzsPQ51lWLObE9VLguNRP5UR0_YV8nNHx-Zime12uExMa3B_JjBNEZ6hyxMZ56UJQ/exec';
