e1ef3ede5864cba302d6c14b2a34250e0b22509f
satriapangestu58 <92577525+satriapangestu58@users.noreply.github.com>
2026-09-05 13:29:16 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BM42_API_URL = 'https://script.google.com/macros/s/AKfycbz9umxwrpHYF3JPs29zV0IESPWL-nkjg88xHjMtjY1sj07JTuFEOD4kEr1eGdwhyIs1/exec';
+window.BM42_API_URL = 'PASTE_APPS_SCRIPT_EXEC_URL_HERE';
